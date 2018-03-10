1b9ce3b8ddd08a2eff014aa9feab586ae98d77fc
superman <bainx@126.com>
2018-03-10 17:29:58 +0800

    test2

--- config/config.default.js
+++ config/config.default.js
@@ -1,12 +1,5 @@
 'use strict';
 
 exports.multipleSequelize = {
-  default: {
-    dialect: 'mysql',
-    database: '',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '',
-  },
+  
 };
